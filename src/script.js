f561bae793820b560ee79ecb376a2e4d39050266
Kevin Tran <kthightran024@gmail.com>
2024-04-07 14:20:44 -0700

    added decrement button and reset button js functions

--- src/script.js
+++ src/script.js
@@ -1,5 +1,24 @@
-const increaseButtonEl = document.querySelector('.counter__button--increase');
 const counterValueEl = document.querySelector('.counter__value');
+const resetButtonEl = document.querySelector('.counter__reset-button');
+const decreaseButtonEl = document.querySelector('.counter__button--decrease');
+const increaseButtonEl = document.querySelector('.counter__button--increase');
+
+resetButtonEl.addEventListener('click', function() {
+  const currentValue = counterValueEl.textContent;
+  const currentValueAsNumber = +currentValue;
+  let newValue = currentValueAsNumber;
+  newValue = 0;
+  
+  counterValueEl.textContent = newValue;
+});
+
+decreaseButtonEl.addEventListener('click', function() {
+  const currentValue = counterValueEl.textContent;
+  const currentValueAsNumber = +currentValue;
+  let newValue = currentValueAsNumber - 1;
+  
+  counterValueEl.textContent = newValue;
+});
 
 increaseButtonEl.addEventListener('click', function() {
   const currentValue = counterValueEl.textContent;
